a93ecd50eb36aa2b4844d3d0c2fb328671682131
SimonYuvarlak <ardayuvarlak@gmail.com>
2019-02-23 19:53:38 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -5,8 +5,8 @@ public class Player {
     private String name;//Name of the player
     private String playerColor;//Color of the player
     private int unitNumber;//Number of the units that the player has
-    private ArrayList <Province> occupiedProvinces;//Occupied places from the player
-    private ArrayList <Card> cards;//Cards that the player has
+    private ArrayList<Province> occupiedProvinces;//Occupied places from the player
+    private ArrayList<Card> cards;//Cards that the player has
 
     //Constructor
     Player(String name, String playerColor, int unitNumber, ArrayList occupiedProvinces, ArrayList cards) {
@@ -92,9 +92,28 @@ public class Player {
         cards.remove(removedCard);
     }
 
+    //This method returns the all possible provinces that can be attacked if there is enough units
+    public ArrayList<Province> availableProvinces(ArrayList<Province> occupiedProvinces){
+        if (occupiedProvinces.size() == 0){return null;}//If there is no province that has occupied
+        else {
+            ArrayList<Province> availableProvinces = new ArrayList<>();//This list will have the all possible provnices to attack for all provinces that are occupied
+            for (Province temp : occupiedProvinces) {//Temp iteratively correspondes to every occupied province
+                for (Province insideTemp : temp.getAdjacentProvinces) {//Getting the adjacent provinces for each occupied provinces from temp by using the variable inseideTemp iteratively
+                    availableProvinces.add(insideTemp);//Adding the province that can be attacked to the created linked list
+                }
+            }
+            return availableProvinces;//Returning the provinces that can be attacked
+        }
+    }
+
     //This methods find a random adjacent province to attack
     public Province findProvinceToAttack(ArrayList provinces){
+        if(this.unitNumber <= 1){
+            return null;
+            System.out.println("Cannot attack due to the lack of unit forces!");
+        }else {
 
+        }
         return null;//Holder
     }
 }
