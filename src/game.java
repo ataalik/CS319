5fad4eaa847d2c73f0b6eeb2cc5b26a81d6b4473
mehmetakifkilic <47590933+mehmetakifkilic@users.noreply.github.com>
2019-04-02 23:07:23 +0300

    Update game.java

--- src/game.java
+++ src/game.java
@@ -4,32 +4,47 @@ import java.util.*;
 //This is the game class
 public class Game {
     private ArrayList<Player> players; //Players of the game
-
+    private Map gameBoard; // map of the game
 
     //Constructor
-    Game(ArrayList<Player> players){
-        players = new ArrayList<>();
+    Game()
+    {
+    	players = new ArrayList<>();
+    	gameBoard = new Map(); 
+        initializePlayers();
     }
+    //initializing players
+    public void initializePlayers()
+    {
+   	 Scanner in = new Scanner(System.in);
+   	 System.out.print("Please enter the number of players:  ");
+   	 int numb = in.nextInt();
+   	 
+   	 for(int i = 0 ; i < numb ; i++)
+   	 {
+   		 System.out.println("Please enter the name of player" + i );
+   		 String name = in.next();
+   		 System.out.println("Please enter the color of player" + i );
+   		 String color = in.next();
+   		 players.add(new Player(name , color));
+   	 }
+   	 // troops will be distributed here
+   }
+   boolean isOver()
+   {
 
-    //getters and setters
+	   ArrayList<Continent> tempContinent = gameBoard.getContinents();
+	   Player temp_player = tempContinent.get(0).getOwner();
+	   
+	   for( Continent continent : tempContinent)
+	   {
+		   if(continent.getOwner() != temp_player )
+		   {
+			   return false;
+		   }
+	   }
+	   return true;
+   }
+  
 
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
-
-    //This method add a new player to the game
-    public void addPlayer(Player newPlayer){
-        this.players.add(newPlayer);
-    }
-
-    //This method creates a new game
-    public void newGame(){
-        for (Player thePlayer: players) {
-            players.add(thePlayer);
-        }
-    }
 }
